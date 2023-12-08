332497f24fb66d3d2ca10c21374e1f77e18e5c6b
sidhanathdon <sid55boss@gmail.com>
2023-12-08 22:23:27 +0530

    change

--- app/components/LocalThemedBox.js
+++ app/components/LocalThemedBox.js
@@ -1,12 +1,28 @@
 'use client'
 import React from 'react';
+import { ThemeContext } from './ThemeProvider';
 
 
 const LocalThemedBox = () => {
+    const {theme}=React.useContext(ThemeContext);
+    const [localTheme,setLocalTheme]=React.useState(theme);
 
+    React.useEffect(()=>{
+        setLocalTheme(theme);
+    },[theme]);
 return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
-        {/* Write code below this line */}
+    <div 
+    className={`bg-${localTheme}`}
+    style={{width:'200px',height:'200px',border:'2px solid green'}} 
+    id="local-themed-box">
+    <p className={`txt-${localTheme}`} id="local-themed-text-container">
+        Some Text
+      </p>
+    <button 
+    className={`btn btn-${localTheme} txt-${localTheme}`} id="local-theme-toggler"
+    onClick={()=>(setLocalTheme(localTheme === "light"? "dark":"light"))}>
+        Toggle local theme to {localTheme === "light"? "dark":"light"}
+        </button>
         
     </div>
 )
--- app/components/ThemeToggleButton.js
+++ app/components/ThemeToggleButton.js
@@ -1,14 +1,18 @@
-'use client'
+'use client';
 import React from 'react';
-
+import { ThemeContext } from './ThemeProvider';
 
 const ThemeToggleButton = () => {
-    
-    return (
-       <>
-       
-       </>
-    )
+    const {theme,toggleTheme}= React.useContext(ThemeContext);
 
-}
+    return (
+       <button 
+       className={`btn btn-${theme} txt-${theme}`}
+       id='global-theme-toggler'
+       onClick={toggleTheme}>
+        Switch to {theme === "light" ? "dark" :"light"} theme
+       </button>
+    );
+
+};
 export {ThemeToggleButton}
